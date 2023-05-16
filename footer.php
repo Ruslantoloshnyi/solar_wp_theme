ffce878654e50322beabc93265bff9ea70e2a73c
Ruslan <ru@skylinehealthgroup.com>
2023-05-16 17:01:14 +0300

    add acf fields to footer

--- footer.php
+++ footer.php
@@ -18,35 +18,25 @@
 		<div class="wrapper">
 			<div class="footer-block">
 				<div class="footer-form">
-					<div class="footer__head">ОСТАВИТЬ ЗАЯВКУ</div>
-					<div class="footer_successfully" style="display: none;">Спасибо, мы свяжемся с вами в ближайшее время</div>
+					<div class="footer__head"><?php echo get_field('footer_heading'); ?></div>
+					<div class="footer_successfully" style="display: none;"><?php echo get_field('footer_successfully'); ?></div>
 					<form class="footer-form__fields" action="" method="POST">
 						<input type="text" name="footer-name" placeholder="Имя">
 						<input type="text" name="footer-phone" placeholder="Телефон">
 						<div class="footer-form__fields-button">
 							<button>&#8594;</button>
-							<p>оставить заявку</p>
+							<p><?php echo get_field('footer_button_text'); ?></p>
 						</div>
 					</form>
 				</div>
 				<div class="footer-contacts">
-					<div class="footer__head">КОНТАКТЫ</div>
-					<div>Центральный офис:<br>
-						г. Москва, ул,Гоголя 13, 1й этаж
-					</div>
-					<div>Представительство в Киеве:<br>
-						г. Киев, ул. Є. Коновальця, 103 оф. 410
-					</div>
-					<div>Представительства в Харькове, Одессе, Ростове, Сочи, Белгороде</div>
-					<div>+7 987 654-54-56<br>
-						+38 067 504-32-57
-					</div>
-					<div>Ежедневно 08:00 - 20:00</div>
+					<div class="footer__head"><?php echo get_field('footer_contacts_heading'); ?></div>
+					<div><?php echo get_field('footer_contacts_content'); ?></div>
 				</div>
 			</div>
 			<div class="footer-social">
 				<a href="#">Политика конфиденциальности</a>
-				<div>SOLAR EXE</div>
+				<div><?php echo get_field('footer_logo'); ?></div>
 				<div class="footer-social__image">
 					<a href="#"><img src="./assets/image/instagram.png" alt=""></a>
 					<a href="#"><img src="./assets/image/telegram.png" alt=""></a>
